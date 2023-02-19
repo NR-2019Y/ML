6b6841a6a70cdb1634bec156cb8c91b9b7911543
NR-2019Y <dbpng@outlook.com>
2023-02-19 18:48:27 +0800

    fast CNN update

--- AUTO_DIFF/examples/run_cnn_mnist.py
+++ AUTO_DIFF/examples/run_cnn_mnist.py
@@ -1,5 +1,5 @@
 import numpy as np
-import datetime
+import datetime, time
 from AUTO_DIFF import my_auto_grad_v0 as op
 from AUTO_DIFF import fast_conv_op_v0 as cnn_op
 from AUTO_DIFF import data_iter
@@ -9,15 +9,15 @@ from datasets.load_img import load_mnist
 
 class CNN:
     def __init__(self):
-        W1 = op.C(np.random.normal(loc=0., scale=0.01, size=[5, 5, 1, 6]), requires_grad=True)
-        b1 = op.C(np.zeros(6), requires_grad=True)
-        W2 = op.C(np.random.normal(loc=0., scale=0.01, size=[5, 5, 6, 16]), requires_grad=True)
-        b2 = op.C(np.zeros(16), requires_grad=True)
+        self.conv1 = cnn_op.Conv2DLayer(out_channels=6, ksize=(5, 5), batch_input_size=(None, 32, 32, 1))
+        self.pool1 = cnn_op.MaxPool2DLayer(ksize=2, batch_input_size=(None, 28, 28, 6), strides=2)
+        self.conv2 = cnn_op.Conv2DLayer(out_channels=16, ksize=(5, 5), batch_input_size=(None, 14, 14, 6))
+        self.pool2 = cnn_op.MaxPool2DLayer(ksize=2, batch_input_size=(None, 10, 10, 16), strides=2)
         n_flatten_features = 5 * 5 * 16
         n_classes = 10
         W3 = op.C(np.random.normal(loc=0., scale=0.01, size=[n_flatten_features, n_classes]), requires_grad=True)
         b3 = op.C(np.zeros(n_classes), requires_grad=True)
-        self.params = W1, b1, W2, b2, W3, b3
+        self.params = self.conv1.params + self.conv2.params + (W3, b3)
 
     def init_grad(self):
         for param in self.params:
@@ -26,12 +26,12 @@ class CNN:
     def __call__(self, X):
         self.init_grad()
         W1, b1, W2, b2, W3, b3 = self.params
-        L1 = op.BroadcastAdd(cnn_op.Conv2D(X, W1), b1)
+        L1 = self.conv1(X)
         L1 = op.Relu(L1)
-        L1 = cnn_op.MaxPool2D(L1, ksize=[2, 2], strides=[2, 2])
-        L2 = op.BroadcastAdd(cnn_op.Conv2D(L1, W2), b2)
+        L1 = self.pool1(L1)
+        L2 = self.conv2(L1)
         L2 = op.Relu(L2)
-        L2 = cnn_op.MaxPool2D(L2, ksize=[2, 2], strides=[2, 2])
+        L2 = self.pool2(L2)
         L_FLATTEN = op.Reshape(L2, (-1, W3._v.shape[0]))
         logits = op.LinearLayer(L_FLATTEN, W3, b3)
         return logits
@@ -60,10 +60,11 @@ c_train_x, c_test_x, c_train_y, c_test_y = map(op.C, (train_x, test_x, train_y, 
 net = CNN()
 trainer = optimizer.Adam(learning_rate=0.001, trainable_nodes=net.params)
 for epoch in range(1, n_epochs + 1):
-
+    tic = time.time()
+    ts = tic
     total_loss = 0.
     nacc = 0.
-    for i, (xi, yi) in enumerate(train_iter):
+    for i, (xi, yi) in enumerate(train_iter, start=1):
         sz = len(yi)
         xi, yi = op.C(xi), op.C(yi)
         logits = net(xi)
@@ -74,7 +75,11 @@ for epoch in range(1, n_epochs + 1):
         total_loss += loss._v * sz
         curr_acc = calc_acc(logits._v, yi._v)
         nacc += curr_acc * sz
-        print(i, loss._v, curr_acc)
+        if i % 5 == 0:
+            te = time.time()
+            tdiff = te - ts
+            ts = te
+            print(f"[{i}] {tdiff}s loss:{loss._v} acc:{curr_acc}")
 
     train_loss = total_loss / len(train_y)
     train_acc = nacc / len(train_y)
@@ -86,5 +91,5 @@ for epoch in range(1, n_epochs + 1):
         logits = net(xi)
         nacc += calc_acc(logits._v, yi._v) * sz
     test_acc = nacc / len(test_y)
-    sdate = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-    print(f"{sdate}\tepoch:{epoch}\tLOSS:{train_loss}\tTRAIN_ACC:{train_acc}\tTEST_ACC:{test_acc}", flush=True)
+    stime = time.time() - tic
+    print(f"epoch:[{epoch}]:{stime}s\tLOSS:{train_loss}\tTRAIN_ACC:{train_acc}\tTEST_ACC:{test_acc}", flush=True)
--- AUTO_DIFF/fast_conv_op_v0.py
+++ AUTO_DIFF/fast_conv_op_v0.py
@@ -1,42 +1,74 @@
 import numbers
 import numpy as np
-from typing import Union, Tuple, List
+from typing import Union, Tuple, List, Optional
 from . import my_auto_grad_v0 as op
+from .layers import Layer
 
 
-# github.com/sebgao/cTensor
-class Conv2D(op.Op):
-    def __init__(self, node1: op.Op, node2: op.Op, *,
-                 strides: Union[int, Tuple[int, ...], List[int]] = 1):
-        X: np.ndarray = node1._v
-        W: np.ndarray = node2._v
-        # X: N, ih, iw, ic
-        # W: kh, kw, ic, oc
-        # Xcol: N, oh, ow, kh, kw, ic
-        # -> N, oh, ow, oc
-        if isinstance(strides, numbers.Number):
+class Conv2DLayer(Layer):
+    def __init__(self, *, out_channels: int,
+                 ksize: Union[int, Tuple[int, int]],
+                 batch_input_size: Tuple[None, int, int, int],
+                 strides: Union[int, Tuple[int, ...], List[int]] = 1,
+                 padding: int = 0, bias: bool = True):
+        if isinstance(ksize, int):
+            ksize = (ksize,) * 2
+        if isinstance(strides, int):
             strides = (strides,) * 2
-        assert len(strides) == 2
-        ih, iw = X.shape[1], X.shape[2]
-        kh, kw = W.shape[0], W.shape[1]
+        assert len(ksize) == 2 and len(strides) == 2
+        _, ih, iw, ic = batch_input_size
+        kh, kw = ksize
         sh, sw = strides
         oh = (ih - kh) // sh + 1
         ow = (iw - kw) // sw + 1
         oshape = (oh, ow, kh, kw)
-        out_im2col_shape = (X.shape[0], oh, ow, kh, kw, X.shape[-1])
+        L = [np.tile(
+            np.tile(np.arange(oshape[i]).reshape(-1, 1), np.prod(oshape[i + 1:], dtype=np.int32)).ravel(),
+            np.prod(oshape[:i], dtype=np.int32)
+        ).reshape(oshape) for i in range(4)]
+        self._strides = strides
+        self._oshape = oshape
+        self._im2col_slice = (slice(None), L[0] * sh + L[2], L[1] * sw + L[3], slice(None))
+        self._padding = padding
+        W = op.C(np.random.normal(loc=0., scale=0.01, size=(kh, kw, ic, out_channels)), requires_grad=True)
+        self._bias = bias
+        if bias:
+            b = op.C(np.zeros(out_channels), requires_grad=True)
+            self.params = (W, b)
+        else:
+            self.params = W,
+
+    def __call__(self, X: op.Op):
+        W = self.params[0]
+        L = _Conv2D(X, W, self._oshape, self._im2col_slice, self._strides, self._padding)
+        if self._bias:
+            b = self.params[-1]
+            return op.AddBiasND(L, b)
+        else:
+            return L
+
+
+# github.com/sebgao/cTensor
+class _Conv2D(op.Op):
+    def __init__(self, node1: op.Op, node2: op.Op,
+                 oshape: Tuple[int, ...],
+                 im2col_slice,
+                 strides: Tuple[int, int] = (1, 1),
+                 padding: int = 0):
+        X: np.ndarray = node1._v
+        W: np.ndarray = node2._v
+        self._padding = padding
+        if padding != 0:
+            X = np.pad(X, pad_width=[[0, 0], [padding, padding], [padding, padding], [0, 0]])
+            self._shape_after_pad = X.shape
+        sh, sw = strides
+        out_im2col_shape = X.shape[:1] + oshape + X.shape[-1:]
         out_im2col_strides = (X.strides[0], X.strides[1] * sh, X.strides[2] * sw,
                               X.strides[1], X.strides[2], X.strides[3])
         Xcol = np.lib.stride_tricks.as_strided(X, out_im2col_shape, out_im2col_strides, writeable=False)
-
-        L = [
-            np.tile(
-                np.tile(np.arange(oshape[i]).reshape(-1, 1), np.prod(oshape[i + 1:], dtype=np.int32)).ravel(),
-                np.prod(oshape[:i], dtype=np.int32)
-            ).reshape(oshape) for i in range(4)
-        ]
-        self._im2col_slice = (slice(None), L[0] * sh + L[2], L[1] * sw + L[3], slice(None))
+        # Xcol = X[im2col_slice] 也能够实现im2col
+        self._im2col_slice = im2col_slice
         self._Xcol = Xcol
-        # Xcol = X[self._im2col_slice], 也能计算Xcol
         self._v = np.tensordot(Xcol, W, axes=[[-3, -2, -1], [0, 1, 2]])
         self._d = 0.
         self.nodes = (node1, node2)
@@ -45,46 +77,70 @@ class Conv2D(op.Op):
         node1, node2 = self.nodes
         W: np.ndarray = node2._v
         if hasattr(node1, "_d"):
-            if isinstance(node1._d, numbers.Number):
-                node1._d = np.zeros_like(node1._v)
             dXcol = np.tensordot(self._d, W, axes=[-1, -1])
-            np.add.at(node1._d, self._im2col_slice, dXcol)
+            if self._padding == 0:
+                if isinstance(node1._d, numbers.Number):
+                    node1._d = np.zeros_like(node1._v)
+                np.add.at(node1._d, self._im2col_slice, dXcol)
+            else:
+                p = self._padding
+                d = np.zeros(shape=self._shape_after_pad)
+                np.add.at(d, self._im2col_slice, dXcol)
+                node1._d += d[:, p:-p, p:-p, :]
         if hasattr(node2, "_d"):
             node2._d += np.tensordot(self._Xcol, self._d, axes=[[0, 1, 2], [0, 1, 2]])
 
 
-class MaxPool2D(op.Op):
-    def __init__(self, node: op.Op, *,
-                 ksize: Union[int, Tuple[int, ...], List[int]] = 1,
-                 strides: Union[int, Tuple[int, ...], List[int]] = 1):
-        # X: N, ih, iw, ic
-        # Xcol: N, oh, ow, kh, kw, ic
-        # -> N, oh, ow, ic
-        if isinstance(ksize, numbers.Number):
+class MaxPool2DLayer(Layer):
+    def __init__(self, *,
+                 ksize: Union[int, Tuple[int, int]],
+                 batch_input_size: Tuple[None, int, int, int],
+                 strides: Union[int, Tuple[int, ...], List[int]] = 1,
+                 padding: int = 0):
+        if isinstance(ksize, int):
             ksize = (ksize,) * 2
-        if isinstance(strides, numbers.Number):
+        if isinstance(strides, int):
             strides = (strides,) * 2
         assert len(ksize) == 2 and len(strides) == 2
-        ih, iw = node.shape[1], node.shape[2]
+        _, ih, iw, ic = batch_input_size
         kh, kw = ksize
         sh, sw = strides
         oh = (ih - kh) // sh + 1
         ow = (iw - kw) // sw + 1
         oshape = (oh, ow, kh, kw)
+        L = [np.tile(
+            np.tile(np.arange(oshape[i]).reshape(-1, 1), np.prod(oshape[i + 1:], dtype=np.int32)).ravel(),
+            np.prod(oshape[:i], dtype=np.int32)
+        ).reshape(oshape) for i in range(4)]
+        self._strides = strides
+        self._oshape = oshape
+        self._im2col_slice = (slice(None), L[0] * sh + L[2], L[1] * sw + L[3], slice(None))
+        self._padding = padding
+
+    def __call__(self, X: op.Op):
+        return _MaxPool2D(X, self._oshape, self._im2col_slice, self._strides, self._padding)
+
+
+class _MaxPool2D(op.Op):
+    def __init__(self, node: op.Op,
+                 oshape: Tuple[int, ...],
+                 im2col_slice,
+                 strides: Tuple[int, int] = (1, 1),
+                 padding: int = 0):
         X: np.ndarray = node._v
-        out_im2col_shape = (X.shape[0], oh, ow, kh, kw, X.shape[-1])
+        self._padding = padding
+        if padding != 0:
+            X = np.pad(X, pad_width=[[0, 0], [padding, padding], [padding, padding], [0, 0]])
+            self._shape_after_pad = X.shape
+        sh, sw = strides
+        out_im2col_shape = X.shape[:1] + oshape + X.shape[-1:]
         out_im2col_strides = (X.strides[0], X.strides[1] * sh, X.strides[2] * sw,
                               X.strides[1], X.strides[2], X.strides[3])
         Xcol = np.lib.stride_tricks.as_strided(X, out_im2col_shape, out_im2col_strides, writeable=False)
-        L = [
-            np.tile(
-                np.tile(np.arange(oshape[i]).reshape(-1, 1), np.prod(oshape[i + 1:], dtype=np.int32)).ravel(),
-                np.prod(oshape[:i], dtype=np.int32)
-            ).reshape(oshape) for i in range(4)
-        ]
+        # Xcol = X[im2col_slice] 也能够实现im2col
+        self._im2col_slice = im2col_slice
         self._Xcol_shape = Xcol.shape
-        self._im2col_slice = (slice(None), L[0] * sh + L[2], L[1] * sw + L[3], slice(None))
-        Xcol_bindkhkw = Xcol.reshape((X.shape[0], oh, ow, -1, X.shape[-1]))
+        Xcol_bindkhkw = Xcol.reshape((Xcol.shape[0], Xcol.shape[1], Xcol.shape[2], -1, Xcol.shape[-1]))
         self._Xcol_bindkhkw_shape = Xcol_bindkhkw.shape
         self._argmax_index = np.expand_dims(Xcol_bindkhkw.argmax(-2), axis=-2)
         self._v = np.take_along_axis(Xcol_bindkhkw, self._argmax_index, axis=-2).squeeze(axis=-2)
@@ -94,8 +150,14 @@ class MaxPool2D(op.Op):
     def back_calc_grad(self):
         node, = self.nodes
         if hasattr(node, "_d"):
-            if isinstance(node._d, numbers.Number):
-                node._d = np.zeros_like(node._v)
             dXcol_bindkhkw = np.zeros(shape=self._Xcol_bindkhkw_shape)
             np.put_along_axis(dXcol_bindkhkw, self._argmax_index, np.expand_dims(self._d, axis=-2), axis=-2)
-            np.add.at(node._d, self._im2col_slice, dXcol_bindkhkw.reshape(self._Xcol_shape))
+            if self._padding == 0:
+                if isinstance(node._d, numbers.Number):
+                    node._d = np.zeros_like(node._v)
+                np.add.at(node._d, self._im2col_slice, dXcol_bindkhkw.reshape(self._Xcol_shape))
+            else:
+                p = self._padding
+                d = np.zeros(shape=self._shape_after_pad)
+                np.add.at(d, self._im2col_slice, dXcol_bindkhkw.reshape(self._Xcol_shape))
+                node._d += d[:, p:-p, p:-p, :]
